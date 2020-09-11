c0b14c1c31d15343642f1872a1f58414aeb81a29
Joshuacodeat10 <54003265+Joshuacodeat10@users.noreply.github.com>
2020-09-11 14:46:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,19 +15,31 @@ function replaceText(element){
    
       
       element.textContent = element.textContent.replace(/Son of a b/gi,'████')
-
+      element.textContent = element.textContent.replace(/cunt/gi,'████')
       element.textContent = element.textContent.replace(/bastard/gi,'████')
-
-
+      element.textContent = element.textContent.replace(/Bloody Hell/gi,'████')
+      element.textContent = element.textContent.replace(/Asshole/gi,'████')
       element.textContent = element.textContent.replace(/fckin/gi,'████')
       element.textContent = element.textContent.replace(/fckin/gi,'████')
+      element.textContent = element.textContent.replace(/fucking/gi,'████')
       element.textContent = element.textContent.replace(/"fckin"/gi,'████')
       element.textContent = element.textContent.replace(/fcking/gi,'████')
-     
+      element.textContent = element.textContent.replace(/bullshit/gi,'████')
+      element.textContent = element.textContent.replace(/goddamnit/gi,'████')
+      element.textContent = element.textContent.replace(/cocaine/gi,'████')
+      element.textContent = element.textContent.replace(/testicle/gi,'████')
+      element.textContent = element.textContent.replace(/damn/gi,'████')
+      element.textContent = element.textContent.replace(/marijuana/gi,'██████')
+      element.textContent = element.textContent.replace(/twat/gi,'██████')
 
-
-
-
+      element.textContent = element.textContent.replace(/minger/gi,'██████')
+      element.textContent = element.textContent.replace(/dickhead/gi,'██████')
+      element.textContent = element.textContent.replace(/fuck of/gi,'██████')
+      element.textContent = element.textContent.replace(/motherfucker/gi,'██████')
+      element.textContent = element.textContent.replace(/goddamn/gi,'██████')
+      element.textContent = element.textContent.replace(/Dick head/gi,'██████')
+      element.textContent = element.textContent.replace(/Dick head/gi,'██████')
+      element.textContent = element.textContent.replace(/Choad/gi,'██████')
 
   }
 }
